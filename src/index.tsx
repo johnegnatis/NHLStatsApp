a0a739f3b776d1f18c7a7c0c294cb7bfc2a89bfe
chrisegnatis <95383207+chrisegnatis@users.noreply.github.com>
2022-02-07 17:56:07 -0600

    Delete React Files

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import { Body } from './components/Body';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { TeamDisplay } from './components/TeamDisplay';
 
@@ -29,9 +28,4 @@ ReactDOM.render(
     </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
